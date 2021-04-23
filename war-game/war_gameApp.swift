09f6e3d0913aebb328e326ab8dae07f8d1351f5b
John Pillsbury <jpillsbu@gmail.com>
2021-04-22 20:30:53 -0400

    Initial Commit

---
+++ war-game/war_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_gameApp.swift
+//  war-game
+//
+//  Created by John Pillsbury on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct war_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
